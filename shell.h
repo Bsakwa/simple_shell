ca9c3b5da48e0cb0253aaa1fafd547d6bd5ac084
Bsakwa <sakwabrian23@gmail.com>
2022-11-13 12:25:49 +0000

    Lists now all read list_e as intended

--- shell.h
+++ shell.h
@@ -15,13 +15,13 @@
 
 /**
  * struct list - linked list for environmental variables
- * @var: holds environmental variable string
+ * @str: holds environmental variable string
  * @next: points to next node
  */
 typedef struct list
 {
-		char *var;
-			struct list *next;
+		char *str;
+		list_e *next;
 
 } list_e;
 
@@ -37,26 +37,26 @@ char *_strcat(char *dest, char *src);
 char *_strdup(char *str);
 char *_strcpy(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
-int _cd(char **str, list_t *env, int num);
-int built_in(char **token, list_t *env, int num, char **command);
-void non_interactive(list_t *env);
-char *_which(char *str, list_t *env);
-int __exit(char **s, list_t *env, int num, char **command);
-int _execve(char *argv[], list_t *env, int num);
+int _cd(char **str, list_e *env, int num);
+int built_in(char **token, list_e *env, int num, char **command);
+void non_interactive(list_e *env);
+char *_which(char *str, list_e *env);
+int __exit(char **s, list_e *env, int num, char **command);
+int _execve(char *argv[], list_e *env, int num);
 void free_double_ptr(char **str);
-void free_linked_list(list_t *list);
-int _env(char **str, list_t *env);
-char *get_env(char *str, list_t *env);
-list_t *env_linked_list(char **env);
-list_t *add_end_node(list_t **head, char *str);
-size_t print_list(list_t *h);
-int delete_nodeint_at_index(list_t **head, int index);
-int _unsetenv(list_t **env, char **str);
-int _setenv(list_t **env, char **str);
-int find_env(list_t *env, char *str);
-void not_found(char *str, int num, list_t *env);
-void cant_cd_to(char *str, int c_n, list_t *env);
-void illegal_number(char *str, int c_n, list_t *env);
+void free_linked_list(list_e *list);
+int _env(char **str, list_e *env);
+char *get_env(char *str, list_e *env);
+list_e *env_linked_list(char **env);
+list_e *add_end_node(list_e **head, char *str);
+size_t print_list(list_e *h);
+int delete_nodeint_at_index(list_e **head, int index);
+int _unsetenv(list_e **env, char **str);
+int _setenv(list_e **env, char **str);
+int find_env(list_e *env, char *str);
+void not_found(char *str, int num, list_e *env);
+void cant_cd_to(char *str, int c_n, list_e *env);
+void illegal_number(char *str, int c_n, list_e *env);
 char *int_to_string(int num);
 
 #endif
